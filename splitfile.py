2d62d5c3ad42d0809f53b2375a23badc2d0f48cd
Joel Stoker <truesilver92@gmail.com>
2012-10-15 16:04:52 -0500

    end of file protection in while loop

--- splitfile.py
+++ splitfile.py
@@ -12,8 +12,17 @@ s_orig = file_orig.read();
 file_orig.close();
 s_len = len(s_orig)
 con = True
+step = 4096
+begin = 0
+end = 0
 
 while (con):
+	end = end + step
+	if (end > s_len - 1):
+		end = s_len - 1
+		con = False
+	
+	
 	
 
 
