7c4780f7cae19241a9993eddfdbaf4dc79d29b77
Joel Stoker <truesilver92@gmail.com>
2012-10-15 16:23:35 -0500

    finished while loop

--- splitfile.py
+++ splitfile.py
@@ -15,13 +15,21 @@ con = True
 step = 4096
 begin = 0
 end = 0
+filecount = 1
 
 while (con):
 	end = end + step
 	if (end > s_len - 1):
 		end = s_len - 1
 		con = False
+	f = open(str(filecount) + '_' + q, 'w', 4096)	
+	s = s_orig[begin:end]
 	
+	f.write(s)
+	f.close()
+	
+	begin = end
+	filecount = filecount + 1	
 	
 	
 
